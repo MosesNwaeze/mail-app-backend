834231b7841cb901746774a5ce3b4303c6d8f68e
Moses Nwaeze <ogbonnasmart@gmail.com>
2024-05-03 20:50:15 +0100

    committing fix to db url

--- src/database/mongodb.config.ts
+++ src/database/mongodb.config.ts
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose, {ConnectOptions} from 'mongoose';
 require('dotenv').config();
 
 const { DB_URL } = process.env;
 
 mongoose
-    .connect(DB_URL as string)
+    .connect(String(DB_URL))
     .then(() => {
         console.log("database successfully connected");
     })

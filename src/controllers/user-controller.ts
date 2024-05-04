e292d6d386beee92e82c599ff173dc2fb817a667
Moses Nwaeze <ogbonnasmart@gmail.com>
2024-05-04 11:28:01 +0100

    committing changes

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -26,3 +26,27 @@ export const users: Handler = async (req: Request, res: Response) => {
     }
 
 }
+
+export const user = async (req: Request, res: Response) => {
+
+    try{
+
+        const {userId} = req.params;
+
+        let user = await Users.findById(userId);
+
+        return res.status(200).json({
+            message: 'User retrieved successfully',
+            status: 'success',
+            data: user
+        })
+
+    }catch (e: any) {
+        console.log('Error fetching user<>', e);
+        return res.status(500).json({
+            message: e.message,
+            status: 'error',
+            error: e
+        })
+    }
+}
--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express';
 import auth from '../middleware/auth';
-import {users} from '../controllers/user-controller';
+import {users,user} from '../controllers/user-controller';
 
 const routes = Router();
 
 routes.get('/',auth,users);
+routes.get('/:userId',auth,user);
 
 export default routes;
